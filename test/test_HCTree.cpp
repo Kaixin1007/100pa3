d498916e5b5b0583cf3bc9f9fa293001dac795b6
Kaixin1007 <kal016@ucsd.edu>
2019-11-04 13:48:47 -0800

    checkpoint

--- test/test_HCTree.cpp
+++ test/test_HCTree.cpp
@@ -11,11 +11,13 @@ using namespace testing;
 class SimpleHCTreeFixture : public ::testing::Test {
   protected:
     HCTree tree;
+    HCTree tree1;
 
   public:
     SimpleHCTreeFixture() {
         // initialization code here
         vector<unsigned int> freqs(256);
+        vector<unsigned int> freqs1(256);
         freqs[','] = 1;
         freqs['p'] = 1;
         freqs['h'] = 2;
@@ -25,6 +27,8 @@ class SimpleHCTreeFixture : public ::testing::Test {
         freqs['a'] = 3;
         freqs['u'] = 9;
         tree.build(freqs);
+        freqs1['a'] = 5;
+        tree1.build(freqs1);
     }
 };
 
@@ -65,5 +69,8 @@ TEST_F(SimpleHCTreeFixture, TEST_DECODE) {
 
     istringstream is2("101");
     ASSERT_EQ(tree.decode(is2), 'a');
+
+    istringstream is3("1111");
+    ASSERT_EQ(tree.decode(is3), 'n');
     cout << endl;
 }
