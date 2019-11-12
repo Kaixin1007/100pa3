009644505bb35a25e4d525c7004c59d4e375e825
Kaixin1007 <kal016@ucsd.edu>
2019-11-11 21:30:59 -0800

    testt

--- src/compress.cpp
+++ src/compress.cpp
@@ -136,9 +136,10 @@ int main(int argc, char* argv[]) {
     // check if argument is vaild
     FileUtils fu;
     if (!fu.isValidFile(inFileName)) return -1;
-
-    if (isAsciiOutput)
-        pseudoCompression(inFileName, outFileName);
-    else
-        trueCompression(inFileName, outFileName);
+    trueCompression(inFileName, outFileName);
+    // if (isAsciiOutput)
+    //     pseudoCompression(inFileName, outFileName);
+    // else
+    //     trueCompression(inFileName, outFileName);
+    return 0;
 }
